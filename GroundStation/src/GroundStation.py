ff91212711d0f2f2f3b1b01c734448b47f1292d3
Niels Joubert <njoubert@gmail.com>
2015-11-01 04:20:19 -0800

    Threaded Blocking implementation

--- GroundStation/src/GroundStation.py
+++ GroundStation/src/GroundStation.py
@@ -9,10 +9,76 @@ add_relative_to_current_source_file_path_to_sys_path("../../")
 
 from spooky import *
 
+#====================================================================#
+
 UDP_SERVER_IP = "127.0.0.1"
 UDP_SERVER_PORT = 19250
 
-class GroundStation:
+#=====================================================================#
+
+import threading
+from Queue import Queue
+
+class GroundStationThreaded:
+  def __init__(self):
+    self.dying = False
+    self.multicast = Multicast()
+    self._udp_send_sbp_TRD  = threading.Thread(target=self._udp_send_sbp_thread)
+    self._udp_send_sbp_TRD.daemon = True
+    self._udp_recv_TRD      = threading.Thread(target=self._ubp_recv_thread)
+    self._udp_recv_TRD.daemon = True
+
+  def _ubp_recv_thread(self):
+    try:
+      while True:
+        data, addr = self.udp.recvfrom(1024)
+        self.multicast.addRecipient(addr)
+        print data, addr
+    except socket.error as (errno, msg):
+      if self.dying:
+        return
+      raise
+
+  def _udp_send_sbp_thread(self):
+    try:
+      while True:
+        self.multicast.sendto(self.udp, "SBP")
+        time.sleep(0.1)
+    except socket.error:
+      if self.dying:
+        return
+      raise
+
+  def stop(self):
+    print ""
+    print "Shutting down"
+    print ""
+    self.dying = True
+    self._udp_send_sbp_TRD.join(0.1)
+    self._udp_recv_TRD.join(0.1)
+
+  def mainloop(self):
+    try:
+
+      with closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as udp:
+        udp.setblocking(1)
+        udp.settimeout(None)
+        udp.bind((UDP_SERVER_IP, UDP_SERVER_PORT))
+        self.udp = udp
+
+        self._udp_send_sbp_TRD.start()
+        self._udp_recv_TRD.start()
+
+        while True:
+          time.sleep(1)
+
+    except KeyboardInterrupt:
+      self.stop()
+
+
+#=====================================================================#
+
+class GroundStationNonblocking:
   def __init__(self):
     self.multicast = Multicast()
     self.scheduler = SimpleScheduler()
@@ -47,10 +113,11 @@ class GroundStation:
       except KeyboardInterrupt:
         pass
 
+#=====================================================================#
 
 def main():
   print "GroundStation"
-  gs = GroundStation()
+  gs = GroundStationThreaded()
   gs.mainloop()
 
 if __name__ == '__main__':
--- OdroidPerson/src/OdroidPerson.py
+++ OdroidPerson/src/OdroidPerson.py
@@ -9,6 +9,7 @@ add_relative_to_current_source_file_path_to_sys_path("../../")
 
 from spooky import *
 
+#====================================================================#
 
 UDP_SERVER_IP = "127.0.0.1"
 UDP_SERVER_PORT = 19250
@@ -16,8 +17,69 @@ UDP_SERVER_PORT = 19250
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
-class OdroidPerson:
+#=====================================================================#
 
+import threading
+from Queue import Queue
+
+class OdroidPersonThreadedBlocking:
+  def __init__(self):
+    self.dying = False
+    self._udp_heartbeat_TRD = threading.Thread(target=self._udp_heartbeat_thread)
+    self._udp_heartbeat_TRD.daemon = True
+    self._udp_recv_TRD      = threading.Thread(target=self._udp_recv_thread)
+    self._udp_recv_TRD.daemon = True
+
+  def _udp_heartbeat_thread(self):
+    while True:
+      try:
+        self.udp.sendto("HEARTBEAT", (UDP_SERVER_IP, UDP_SERVER_PORT))
+        time.sleep(0.1)
+      except socket.error:
+        if self.dying:
+          return
+        raise
+
+  def _udp_recv_thread(self):
+    while True:    
+      try:
+        data, addr = self.udp.recvfrom(1024)
+        print data, addr   
+      except socket.error:
+        if self.dying:
+          return
+        raise 
+
+  def stop(self):
+    print ""
+    print "Shutting down"
+    print ""
+    self.dying = True
+    self._udp_heartbeat_TRD.join(0.1)
+    self._udp_recv_TRD.join(0.1)
+
+  def mainloop(self):
+    try:
+
+      with closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as udp:
+        udp.setblocking(1)
+#        udp.settimeout(None)
+        udp.bind((UDP_IP, UDP_PORT))
+        self.udp = udp
+
+        self._udp_heartbeat_TRD.start()
+        self._udp_recv_TRD.start()
+
+        while True:
+          time.sleep(1)
+
+    except KeyboardInterrupt:
+      self.stop()
+
+
+#=====================================================================#
+
+class OdroidPersonNonblocking:
   def __init__(self):
     self.scheduler = SimpleScheduler()
 
@@ -47,10 +109,11 @@ class OdroidPerson:
       except KeyboardInterrupt:
         pass
 
+#=====================================================================#
 
 def main():
   print "OdroidPerson"
-  op = OdroidPerson()
+  op = OdroidPersonThreadedBlocking()
   op.mainloop()
 
 if __name__ == '__main__':
