db5ca5df46bb9fe4e30c04026bff5eb15eddb75b
Niels Joubert <njoubert@gmail.com>
2015-11-18 23:20:10 -0800

    attempt ot parse again

--- src/odroidperson/main.py
+++ src/odroidperson/main.py
@@ -44,7 +44,7 @@ logger.addHandler(ch)
 class SBPUDPBroadcastDriver(BaseDriver):
 
   def __init__(self, bind_port):
-    self.handle = spooky.BufferedUDPBroadcastSocket(port=self.bind_port)
+    self.handle = spooky.BufferedUDPBroadcastSocket(port=bind_port)
     self.last_addr = None
     BaseDriver.__init__(self, self.handle)
 
