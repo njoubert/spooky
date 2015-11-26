67aaee5d6cbbf6901b29e8b43e5cb09879aca7f8
Niels Joubert <njoubert@gmail.com>
2015-11-25 21:35:33 -0800

    stripped down, cleaned up install and update script

--- bin/install.sh
+++ bin/install.sh
@@ -4,24 +4,37 @@ ABS_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && cd .. && pwd)
 
 cd ${ABS_PATH%%/}/etc/
 
-#
-#cp spooky.conf /etc/init/spooky.conf
-#initctl reload-configuration
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [[ $1 = 'remove' ]]
+then
+
+	echo 'removing spooky odroidperson systemd service'
+
+	systemctl stop spooky.service && \
+		systemctl disable spooky.service && \
+		rm -f /lib/systemd/system/network-online.target.wants/spooky.service && \
+		rm -f /lib/systemd/system/spooky.service && \
+		systemctl daemon-reload
+
+else
+
+	echo 'installing spooky odroidperson systemd service'
+
+	mkdir -p /logs && \
+		mkdir -p /tmp && \
+		cp spooky.service /lib/systemd/system/spooky.service && \
+		ln -s /lib/systemd/system/spooky.service /lib/systemd/system/network-online.target.wants/ && \
+		systemctl daemon-reload && \
+		systemctl enable spooky.service
+		systemctl start spooky.service
+
+fi
 
 
-mkdir -p /logs && \
-	mkdir -p /tmp && \
-	cp spooky.service /lib/systemd/system/spooky.service && \
-	ln -s /lib/systemd/system/spooky.service /lib/systemd/system/network-online.target.wants/ && \
-	systemctl daemon-reload && \
-	systemctl enable spooky.service
 
 
 
-
-# sudo systemctl stop spooky.service
-# sudo systemctl disable spooky.service
-# sudo rm -f /lib/systemd/system/network-online.target.wants/spooky.service
-# sudo systemctl disable NetworkManager-wait-online.service
-# sudo systemctl disable systemd-networkd-wait-online.service
-# sudo rm -f /lib/systemd/system/spooky.service
--- bin/update-and-restart.sh
+++ bin/update-and-restart.sh
@@ -4,11 +4,6 @@ ABS_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && cd .. && pwd)
 
 cd ${ABS_PATH%%/}
 
-#
-#cp spooky.conf /etc/init/spooky.conf
-#initctl reload-configuration
-
-
 systemctl stop spooky.service
 git pull
 ./bin/install.sh
