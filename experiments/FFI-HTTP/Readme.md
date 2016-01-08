faa41cfc70d20b68b0da3507903cb7f516192035
Niels Joubert <njoubert@gmail.com>
2016-01-08 15:44:20 -0800

    Experiment: FFI-HTTP

---
+++ experiments/FFI-HTTP/Readme.md
@@ -0,0 +1,6 @@
+# Foreign Function Interface to Python using JSON over HTTP
+
+Install Flask
+
+	pip install Flask
+
---
+++ experiments/FFI-HTTP/main.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2016 Stanford University
+# Contact: Niels Joubert <niels@cs.stanford.edu>
+#
+
+from flask import Flask
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
