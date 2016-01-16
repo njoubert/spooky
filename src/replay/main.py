e15cd5772c4493dcbf0cfb00ebc9eafba276f9f7
Niels Joubert <njoubert@gmail.com>
2016-01-16 01:26:36 -0800

    REPLAY additions: end points, speedup, and useful debug info

--- src/replay/main.py
+++ src/replay/main.py
@@ -22,7 +22,9 @@ def replay_log(logfile, dest,
     startTime=0.0, 
     debug=False, 
     infoOnly=False, 
-    loop=True):
+    loop=True,
+    speedup=1.0,
+    end=0.0):
   print "Replaying log %s to %s" % (logfile, str(dest))
   pp = pprint.PrettyPrinter(indent=1)
 
@@ -36,9 +38,25 @@ def replay_log(logfile, dest,
     if infoOnly:
       print "Log info:"
       try:
+        minN = 10000000
+        minE = 10000000
+        maxN = -1000000
+        maxE = -1000000
         while True:      
           state = pickle.load(f)
+          data = json.dumps(state)
+          for person in ["192.168.2.51", "192.168.2.52"]:
+            if person in state:
+              if "MsgBaselineNED" in state[person]:
+                minN = min(minN, state[person]["MsgBaselineNED"]["n"])
+                minE = min(minE, state[person]["MsgBaselineNED"]["e"])
+                maxN = max(maxN, state[person]["MsgBaselineNED"]["n"])
+                maxE = max(maxE, state[person]["MsgBaselineNED"]["e"])
+
       finally:
+        print "  boundaries: "
+        print "    North: [%d mm, %d mm] " % (minN, maxN)
+        print "    East:  [%d mm, %d mm] " % (minE, maxE)
         print "  Log created on %s" % time.ctime(os.path.getctime(logfile))
         print "  Log length is %.2fs " % (state['_timestamp'] - firstStamp)
         sys.exit(1)
@@ -48,11 +66,16 @@ def replay_log(logfile, dest,
         state_udp_out.setblocking(1)
 
         while True:
+
           # FFWD:
-          if startTime > 0.0:
-            print "Fast Forwarding to %.2fs" % startTime
-            while (state['_timestamp'] - firstStamp) <= startTime:
-              state = pickle.load(f)
+          try:
+            if startTime > 0.0:
+              print "Fast Forwarding to %.2fs" % startTime
+              while (state['_timestamp'] - firstStamp) <= startTime:
+                state = pickle.load(f)
+          except EOFError:
+            print "Specified start point beyond end of file"
+            sys.exit(0)
 
           # SEND:
           try:
@@ -60,6 +83,11 @@ def replay_log(logfile, dest,
             while True:
                 
                 nextState = pickle.load(f)
+
+                if "solo" in nextState:
+                  del nextState["solo"]
+                if "solo_sbp" in nextState:
+                  del nextState["solo_sbp"]
 
                 data = json.dumps(state)
                 try:
@@ -72,20 +100,24 @@ def replay_log(logfile, dest,
                 if debug:
                   pp.pprint(state)
 
-                timediff = nextState['_timestamp'] - state['_timestamp']
+                timediff = (nextState['_timestamp'] - state['_timestamp'])/speedup
                 startdiff = state['_timestamp'] - firstStamp
+                if (end > 0.0 and startdiff > end):
+                  break
                 print("Sending state %.2fs from log beginning, then sleeping for %.3fs" % (startdiff, timediff))
                 state = nextState
                 time.sleep(timediff)
               
           except EOFError:
+            if not loop:
+              print "Done"
+              sys.exit(0)
+
+          finally:
             if loop:
               print "Looping..."
               f.seek(0)
               state = pickle.load(f) 
-            else:
-              print "Done"
-              sys.exit(0)
     except KeyboardInterrupt:
       print ""
       print ""
@@ -112,6 +144,12 @@ def main():
   parser.add_argument("-s", "--start",
                       default=[0.0], nargs=1, type=float,
                       help="specify the port to send the log to")
+  parser.add_argument("-e", "--end",
+                      default=[0.0], nargs=1, type=float,
+                      help="specify the port to send the log to")
+  parser.add_argument("-x", "--speedup",
+                      default=[1.0], nargs=1, type=float,
+                      help="specify a speedup factor.")
   parser.add_argument("--dontloop",
                       action="store_true",
                       help="don't loop the log")
@@ -119,7 +157,7 @@ def main():
                       help="specify the logfile to load",)
   args = parser.parse_args()
 
-  replay_log(args.log[0],(args.ip[0],args.port[0]), startTime=args.start[0], debug=args.debug, infoOnly=args.info,loop=(not args.dontloop))
+  replay_log(args.log[0],(args.ip[0],args.port[0]), startTime=args.start[0], debug=args.debug, infoOnly=args.info,loop=(not args.dontloop), speedup=args.speedup[0], end=args.end[0])
 
 if __name__ == '__main__':
   main()
